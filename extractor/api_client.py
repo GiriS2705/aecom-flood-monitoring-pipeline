68c7f491af4ed963679a7fb9cd1d326716769741
GiriS2705 <mail2giritharans.com>
2026-07-08 22:22:42 +0100

    Implement API ingestion pipeline and Snowflake Bronze layer

--- extractor/api_client.py
+++ extractor/api_client.py
@@ -2,18 +2,27 @@ import requests
 
 BASE_URL = "https://environment.data.gov.uk/flood-monitoring"
 
+
 def get_all_stations():
-    url = f"{BASE_URL}/id/stations"
-
-    response = requests.get(url, timeout=30)
+    response = requests.get(f"{BASE_URL}/id/stations", timeout=30)
     response.raise_for_status()
-
     return response.json()
 
-def get_latest_readings():
-    url = f"{BASE_URL}/data/readings"
 
-    response = requests.get(url, timeout=30)
+def get_readings(start_date, end_date):
+
+    params = {
+        "startdate": start_date,
+        "enddate": end_date,
+        "_limit": 10000
+    }
+
+    response = requests.get(
+        f"{BASE_URL}/data/readings",
+        params=params,
+        timeout=60
+    )
+
     response.raise_for_status()
 
     return response.json()
--- extractor/main.py
+++ extractor/main.py
@@ -1,18 +1,48 @@
-from api_client import get_all_stations
+from datetime import datetime, timedelta, UTC
+
+from api_client import get_all_stations, get_readings
 from s3_upload import upload_json
-from datetime import datetime
+
 
 def main():
+    # Current date for S3 partitioning
+    today = datetime.now(UTC)
 
-    print("Fetching monitoring stations...")
+    # Date range for one week's data
+    end_date = today.strftime("%Y-%m-%d")
+    start_date = (today - timedelta(days=7)).strftime("%Y-%m-%d")
 
+    print(f"Fetching stations...")
     stations = get_all_stations()
 
-    today = datetime.utcnow().strftime("%Y/%m/%d")
+    station_key = (
+        f"flood-monitoring/raw/stations/"
+        f"year={today.year}/"
+        f"month={today.strftime('%m')}/"
+        f"day={today.strftime('%d')}/"
+        f"stations.json"
+    )
 
-    object_key = f"flood-monitoring/raw/{today}/stations.json"
+    upload_json(stations, station_key)
 
-    upload_json(stations, object_key)
+    print(f"Fetching readings from {start_date} to {end_date}...")
+
+    readings = get_readings(start_date, end_date)
+
+    print(f"Total Readings Retrieved: {len(readings['items'])}")
+
+    reading_key = (
+        f"flood-monitoring/raw/readings/"
+        f"year={today.year}/"
+        f"month={today.strftime('%m')}/"
+        f"day={today.strftime('%d')}/"
+        f"readings.json"
+    )
+
+    upload_json(readings, reading_key)
+
+    print("\nPipeline completed successfully.")
+
 
 if __name__ == "__main__":
     main()
